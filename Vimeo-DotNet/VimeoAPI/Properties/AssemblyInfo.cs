9ab7807d0a3dd9519b9501361cba1c66bf8d314e
Jason Duncan <j.duncan@techsmith.com>
2015-09-11 11:45:46 -0400

    Update version

--- Vimeo-DotNet/VimeoAPI/Properties/AssemblyInfo.cs
+++ Vimeo-DotNet/VimeoAPI/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("VimeoAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Breaking Changes: v2.0.0 - Updgraded to VS2015, .NET 4.6")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("VimeoAPI")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
